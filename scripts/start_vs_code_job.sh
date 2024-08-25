85e3fcf9483c805de0fd284f06e5af1bc22bcba0
adkostic <adkostic@gmail.com>
2024-08-25 10:28:40 -0400

    minor script update

--- scripts/start_vs_code_job.sh
+++ scripts/start_vs_code_job.sh
@@ -3,11 +3,11 @@
 #SBATCH -p priority
 #SBATCH -c 12
 #SBATCH --mem=32G
-#SBATCH -t 02:00:00 #### -t 1-12:00:00  # Specifies 1 day and 12 hours
+#SBATCH -t 14:00:00 #### -t 1-12:00:00  # Specifies 1 day and 12 hours
 #SBATCH --job-name="vscodetunnel"
 
 # Load necessary modules if needed
 # ~/.bashrc
 
 # Sleep for the duration of the job
-sleep 2h  # Adjust as needed # sleep 2d  # Sleeps for 2 days  sleep 30m for 30 minutes, etc.
+sleep 14h  # Adjust as needed # sleep 2d  # Sleeps for 2 days  sleep 30m for 30 minutes, etc.
