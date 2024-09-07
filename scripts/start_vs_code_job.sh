f68f4f307dbf52ba9b78d128632f69185ccd8ee6
adkostic <adkostic@gmail.com>
2024-09-07 17:43:38 -0400

    minor script mod

--- scripts/start_vs_code_job.sh
+++ scripts/start_vs_code_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH -p priority
-#SBATCH -c 6
-#SBATCH --mem=16G
+#SBATCH -c 10
+#SBATCH --mem=32G
 #SBATCH -t 12:00:00 #### -t 1-12:00:00  # Specifies 1 day and 12 hours
 #SBATCH --job-name="vscodetunnel"
 
